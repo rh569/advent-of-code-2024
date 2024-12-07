4160395886717cef45fb4afdfc06e51a83e37018
Ryan Hadlow <22102278+rh569@users.noreply.github.com>
2024-12-07 22:13:59 +0000

    feat(day6,part2): Guard Gallivant

--- src/days/day6.ts
+++ src/days/day6.ts
@@ -1,10 +1,42 @@
 import { makeMessageHandler } from "../libs/worker"
 
+type Pos = [number, number]
 const DIR_ORDER = ['up', 'right', 'down', 'left']
 
 function part1(input: string) {
-    let guardPos: [number, number] | null = null
-    const obstaclePos: [number, number][] = []
+    const [guardPos, obstaclePos, grid] = parseInput(input)
+
+    const visited = simulateGuard(guardPos, obstaclePos, grid[0].length)
+    logGrid(grid, visited)
+
+    return visited.size
+}
+
+function part2(input: string) {
+    const [guardPos, obstaclePos, grid] = parseInput(input)
+
+    const visited = simulateGuard(guardPos, obstaclePos, grid[0].length)
+    visited.delete(`${guardPos}`)
+
+    let loopVariantCount = 0
+
+    for (const vPosStr of visited) {
+        // loop over all visited points bar the start and alter the grid to include obs there
+        const newObstaclePos = vPosStr.split(',').map(Number) as Pos
+        const gridCopy = copyGrid(grid)
+
+        //set new obstacle
+        gridCopy[newObstaclePos[0]][newObstaclePos[1]] = '#'
+
+        if (pathHasLoop(guardPos, gridCopy)) loopVariantCount++
+    }
+
+    return loopVariantCount
+}
+
+function parseInput(input: string): [Pos, Pos[], string[][]] {
+    let guardPos: Pos | null = null
+    const obstaclePos: Pos[] = []
 
     const grid = input.split('\n')
         .map((l, i) => l.split('').map((c, j) => {
@@ -15,19 +47,10 @@ function part1(input: string) {
 
     if (!guardPos) throw new Error('Found no guard!')
 
-    const visted = simulateGuard(guardPos, obstaclePos, grid[0].length)
-
-    logGrid(grid, visted)
-
-    return visted.size
+    return [guardPos, obstaclePos, grid]
 }
 
-// TODO
-function part2(input: string) {
-    return input.length
-}
-
-function simulateGuard(guardPos: [number, number], obstaclePos: [number, number][], gridLength: number): Set<string> {
+function simulateGuard(guardPos: Pos, obstaclePos: Pos[], gridLength: number): Set<string> {
     const visited = new Set([`${guardPos}`])
 
     let pos = guardPos
@@ -46,11 +69,11 @@ function simulateGuard(guardPos: [number, number], obstaclePos: [number, number]
         pos = getNextPos(obstacle, dir)
         dir = getNextDir(dir)
     }
-    
+
     return visited
 }
 
-function getNextObstacle(pos: [number, number], dir: string, obstaclePos: [number, number][]): [number, number] | null {
+function getNextObstacle(pos: Pos, dir: string, obstaclePos: Pos[]): Pos | null {
     const obstaclesInPath = getObstaclesInPath(pos, dir, obstaclePos)
     if (obstaclesInPath.length === 0) return null
 
@@ -68,7 +91,7 @@ function getNextObstacle(pos: [number, number], dir: string, obstaclePos: [numbe
     }, obstaclesInPath[0])
 }
 
-function getObstaclesInPath(pos: [number, number], dir: string, obstaclePos: [number, number][]) {
+function getObstaclesInPath(pos: Pos, dir: string, obstaclePos: Pos[]) {
     return obstaclePos.filter(([oi, oj]) => {
         switch (dir) {
             case 'up':
@@ -83,17 +106,17 @@ function getObstaclesInPath(pos: [number, number], dir: string, obstaclePos: [nu
     })
 }
 
-function getNextPos(oPos: [number, number], dir: string): [number, number] {
+function getNextPos(oPos: Pos, dir: string): Pos {
     const [i, j] = oPos;
     switch (dir) {
         case 'up':
-            return [i+1, j]
+            return [i + 1, j]
         case 'right':
-            return [i, j-1]
+            return [i, j - 1]
         case 'down':
-            return [i-1, j]
+            return [i - 1, j]
         default:
-            return [i, j+1]
+            return [i, j + 1]
     }
 }
 
@@ -102,64 +125,110 @@ function getNextDir(d: string): string {
     return DIR_ORDER[(i + 1) % DIR_ORDER.length]
 }
 
-function visitPointsToEdge(visited: Set<string>, pos: [number, number], dir: string, gridLength: number): void {
+function visitPointsToEdge(visited: Set<string>, pos: Pos, dir: string, gridLength: number): void {
     const [pi, pj] = pos
     switch (dir) {
         case 'up':
             for (let i = pi; i >= 0; i--) {
-                visited.add(`${[i,pj]}`)
+                visited.add(`${[i, pj]}`)
             }
             break
         case 'right':
             for (let j = pj; j < gridLength; j++) {
-                visited.add(`${[pi,j]}`)
+                visited.add(`${[pi, j]}`)
             }
             break
         case 'down':
             for (let i = pi; i < gridLength; i++) {
-                visited.add(`${[i,pj]}`)
+                visited.add(`${[i, pj]}`)
             }
             break
         default:
             for (let j = pj; j >= 0; j--) {
-                visited.add(`${[pi,j]}`)
+                visited.add(`${[pi, j]}`)
             }
             break
     }
 }
 
-function visitPointsToObstacle(visited: Set<string>, pos: [number, number], dir: string, obstacle: [number, number]): void {
+function visitPointsToObstacle(visited: Set<string>, pos: Pos, dir: string, obstacle: Pos): void {
     const [pi, pj] = pos
     const [oi, oj] = obstacle
     switch (dir) {
         case 'up':
             for (let i = pi; i > oi; i--) {
-                visited.add(`${[i,pj]}`)
+                visited.add(`${[i, pj]}`)
             }
             break
         case 'right':
             for (let j = pj; j < oj; j++) {
-                visited.add(`${[pi,j]}`)
+                visited.add(`${[pi, j]}`)
             }
             break
         case 'down':
             for (let i = pi; i < oi; i++) {
-                visited.add(`${[i,pj]}`)
+                visited.add(`${[i, pj]}`)
             }
             break
         default:
             for (let j = pj; j > oj; j--) {
-                visited.add(`${[pi,j]}`)
+                visited.add(`${[pi, j]}`)
             }
             break
     }
+}
+
+function pathHasLoop(start: Pos, grid: string[][]): boolean {
+    const directionsByVisitedPositions = new Map<string, string[]>()
+
+    let currentDirection = 'up'
+    let currentPosition = start
+
+    while (true) {
+        const visitedDirs = directionsByVisitedPositions.get(`${currentPosition}`)
+        // already been here while facing this way, so must be in a loop
+        if (visitedDirs?.includes(currentDirection)) return true
+
+        const facingPos = getFacingPos(currentPosition, currentDirection)
+
+        // check if we reached the edge
+        if (!isInBounds(facingPos, grid.length)) return false
+
+        // turn / move
+        if (grid[facingPos[0]][facingPos[1]] === '#') {
+            currentDirection = getNextDir(currentDirection)
+        } else {
+            directionsByVisitedPositions.set(`${currentPosition}`,
+                [...(directionsByVisitedPositions.get(`${currentPosition}`) ?? []), currentDirection]
+            )
+            currentPosition = facingPos
+        }
+    }
+}
+
+function getFacingPos(pos: Pos, direction: string): Pos {
+    switch (direction) {
+        case 'up':
+            return [pos[0] - 1, pos[1]]
+        case 'right':
+            return [pos[0], pos[1] + 1]
+        case 'down':
+            return [pos[0] + 1, pos[1]]
+        default:
+            return [pos[0], pos[1] - 1]
+    }
+}
+
+function isInBounds(facingPos: Pos, size: number): boolean {
+    return facingPos[0] >= 0 && facingPos[0] < size
+        && facingPos[1] >= 0 && facingPos[1] < size
 }
 
 function logGrid(grid: string[][], visited: Set<string>) {
     for (let i = 0; i < grid.length; i++) {
         let line = '';
         for (let j = 0; j < grid[0].length; j++) {
-            if (visited.has(`${[i,j]}`)) {
+            if (visited.has(`${[i, j]}`)) {
                 if (grid[i][j] === '#') {
                     line += 'X'
                 } else {
@@ -171,6 +240,16 @@ function logGrid(grid: string[][], visited: Set<string>) {
         }
         console.log(line)
     }
+}
+
+function copyGrid(grid: string[][]): string[][] {
+    const copy: string[][] = []
+
+    for (const row of grid) {
+        copy.push([...row])
+    }
+
+    return copy
 }
 
 onmessage = makeMessageHandler(part1, part2)
